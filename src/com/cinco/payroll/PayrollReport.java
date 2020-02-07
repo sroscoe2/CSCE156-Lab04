3d85646ed2ac0017fb976ef996fd0b33afa34a4a
Hundter Biede <6586509+hbiede@users.noreply.github.com>
2020-02-07 11:01:29 -0600

    Minimize code in the try-block

--- src/com/cinco/payroll/PayrollReport.java
+++ src/com/cinco/payroll/PayrollReport.java
@@ -1,6 +1,7 @@
 package com.cinco.payroll;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,41 +10,43 @@ public class PayrollReport {
 
 	public static List<Employee> parseDataFile() {
 		List<Employee> result = new ArrayList<Employee>();
+		File f = new File("data/employee.dat");
+		Scanner s;
 		try {
-			File f = new File("data/employee.dat");
-			Scanner s = new Scanner(f);
-			while(s.hasNext()) {
-				String line = s.nextLine();
-				if(!line.trim().isEmpty()) {
-					Employee e = null;
-					String tokens[] = line.split(";");
-					String id = tokens[1];
-					String nameTokens[] = tokens[2].split(",");
-					String lastName = nameTokens[0];
-					String firstName = nameTokens[1];
-					String title = tokens[3];
-					double annualSalary = 0.0, hourlyPayRate = 0.0, hoursWorked = 0.0;
-					if(tokens.length == 6) {
-						hourlyPayRate = Double.parseDouble(tokens[4]);
-						hoursWorked = Double.parseDouble(tokens[5]);
-					} else if(tokens.length == 5) {
-						annualSalary = Double.parseDouble(tokens[4]);
-					}
-					
-					if(tokens[0].equals("E")) {
-						e = new Employee(); //TODO: modify this
-					} else if(tokens[0].equals("S")) {
-						e = new Employee(); //TODO: modify this
-					} else if(tokens[0].equals("T")) {
-						e = new Employee(); //TODO: modify this
-					}
-					
-					result.add(e);
-				}
-			}
-		} catch(Exception e) {
-			throw new RuntimeException(e);
+			s = new Scanner(f);
+		} catch(FileNotFoundException fnfe) {
+			throw new RuntimeException(fnfe);
 		}
+		while(s.hasNext()) {
+			String line = s.nextLine();
+			if(!line.trim().isEmpty()) {
+				Employee e = null;
+				String tokens[] = line.split(";");
+				String id = tokens[1];
+				String nameTokens[] = tokens[2].split(",");
+				String lastName = nameTokens[0];
+				String firstName = nameTokens[1];
+				String title = tokens[3];
+				double annualSalary = 0.0, hourlyPayRate = 0.0, hoursWorked = 0.0;
+				if(tokens.length == 6) {
+					hourlyPayRate = Double.parseDouble(tokens[4]);
+					hoursWorked = Double.parseDouble(tokens[5]);
+				} else if(tokens.length == 5) {
+					annualSalary = Double.parseDouble(tokens[4]);
+				}
+				
+				if(tokens[0].equals("E")) {
+					e = new Employee(); //TODO: modify this
+				} else if(tokens[0].equals("S")) {
+					e = new Employee(); //TODO: modify this
+				} else if(tokens[0].equals("T")) {
+					e = new Employee(); //TODO: modify this
+				}
+				
+				result.add(e);
+			}
+		}
+		
 		return result;
 	}
 	
